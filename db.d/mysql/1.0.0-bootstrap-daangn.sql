3e96553e7c7f2c07004107abaa93b2f6f459e0f0
silver <109951843+silverlee425@users.noreply.github.com>
2024-06-21 17:10:51 +0900

    Update 1.0.0-bootstrap-daangn.sql

--- db.d/mysql/1.0.0-bootstrap-daangn.sql
+++ db.d/mysql/1.0.0-bootstrap-daangn.sql
@@ -1,77 +1,75 @@
-CREATE TABLE `model` (
+CREATE TABLE `Model` (
   `id` enum('1') NOT NULL,
   `version` VARCHAR(20) NOT NULL,
   PRIMARY KEY (`id`)
 );
-INSERT INTO `model` (`version`) VALUES ("1.0.0");
+INSERT INTO `Model` (`version`) VALUES ("1.0.0");
 
 -- Create the rest of the tables
-CREATE TABLE `alert_group` (
-	`id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE `AlertGroup` (
+	`ID` INT NOT NULL AUTO_INCREMENT,
 	`time` TIMESTAMP NOT NULL,
 	`receiver` VARCHAR(100) NOT NULL,
 	`status` VARCHAR(50) NOT NULL,
-	`external_url` TEXT NOT NULL,
-	`group_key` VARCHAR(255) NOT NULL,
-	`created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-	KEY `ix_time` (`time`),
-    	KEY `ix_status_time` (`status`, `time`),
-	PRIMARY KEY (`id`)
-);
+	`externalURL` TEXT NOT NULL,
+	`groupKey` VARCHAR(255) NOT NULL,
+	KEY `idx_time` (`time`) USING BTREE,
+    KEY `idx_status_ts` (`status`, `time`) USING BTREE,
+	PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `group_label` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-    	`alert_group_id` INT NOT NULL,
-    	`group_label` VARCHAR(100) NOT NULL,
-    	`value` VARCHAR(1000) NOT NULL,
-    	`created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`)
-);
+CREATE TABLE `GroupLabel` (
+	`ID` INT NOT NULL AUTO_INCREMENT,
+    `AlertGroupID` INT NOT NULL,
+    `GroupLabel` VARCHAR(100) NOT NULL,
+    `Value` VARCHAR(1000) NOT NULL,
+    FOREIGN KEY (AlertGroupID) REFERENCES AlertGroup (ID) ON DELETE CASCADE,
+	PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `common_label` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-    	`alert_group_id` INT NOT NULL,
-    	`label` VARCHAR(100) NOT NULL,
-    	`value` VARCHAR(1000) NOT NULL,
-    	`created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`)
-);
+CREATE TABLE `CommonLabel` (
+	`ID` INT NOT NULL AUTO_INCREMENT,
+    `AlertGroupID` INT NOT NULL,
+    `Label` VARCHAR(100) NOT NULL,
+    `Value` VARCHAR(1000) NOT NULL,
+    FOREIGN KEY (AlertGroupID) REFERENCES AlertGroup (ID) ON DELETE CASCADE,
+	PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `common_annotation` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-    	`alert_group_id` INT NOT NULL,
-    	`annotation` VARCHAR(100) NOT NULL,
-    	`value` VARCHAR(1000) NOT NULL,
-    	`created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`)
-);
+CREATE TABLE `CommonAnnotation` (
+	`ID` INT NOT NULL AUTO_INCREMENT,
+    `AlertGroupID` INT NOT NULL,
+    `Annotation` VARCHAR(100) NOT NULL,
+    `Value` VARCHAR(1000) NOT NULL,
+    FOREIGN KEY (AlertGroupID) REFERENCES AlertGroup (ID) ON DELETE CASCADE,
+	PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `alert` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-    	`alert_group_id` INT NOT NULL,
+CREATE TABLE `Alert` (
+	`ID` INT NOT NULL AUTO_INCREMENT,
+    `alertGroupID` INT NOT NULL,
 	`status` VARCHAR(50) NOT NULL,
-    	`starts_at` DATETIME NOT NULL,
-    	`ends_at` DATETIME DEFAULT NULL,
-	`generator_url` TEXT NOT NULL,
-	`fingerprint` TEXT NOT NULL,
-	`created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`)
-);
+    `startsAt` DATETIME NOT NULL,
+    `endsAt` DATETIME DEFAULT NULL,
+	`generatorURL` TEXT NOT NULL,
+    FOREIGN KEY (alertGroupID) REFERENCES AlertGroup (ID) ON DELETE CASCADE,
+	PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `alert_label` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-    	`alert_id` INT NOT NULL,
-    	`label` VARCHAR(100) NOT NULL,
-    	`value` VARCHAR(1000) NOT NULL,
-    	`created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`)
-);
+CREATE TABLE `AlertLabel` (
+	`ID` INT NOT NULL AUTO_INCREMENT,
+    `AlertID` INT NOT NULL,
+    `Label` VARCHAR(100) NOT NULL,
+    `Value` VARCHAR(1000) NOT NULL,
+    FOREIGN KEY (AlertID) REFERENCES Alert (ID) ON DELETE CASCADE,
+	PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `alert_annotation` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-    	`alert_id` INT NOT NULL,
-    	`annotation` VARCHAR(100) NOT NULL,
-    	`value` VARCHAR(1000) NOT NULL,
-    	`created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`)
-);
+CREATE TABLE `AlertAnnotation` (
+	`ID` INT NOT NULL AUTO_INCREMENT,
+    `AlertID` INT NOT NULL,
+    `Annotation` VARCHAR(100) NOT NULL,
+    `Value` VARCHAR(1000) NOT NULL,
+    FOREIGN KEY (AlertID) REFERENCES Alert (ID) ON DELETE CASCADE,
+	PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
